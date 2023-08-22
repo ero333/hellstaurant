33e8692ecc8506565b6fc7f2050c0d64ab8eb289
Agostinaluparello <agostina.luparello@usal.edu.ar>
2023-08-22 15:24:25 -0300

    +spawn de enemigos

--- TADDIV - Hellstaurant/Assets/SCRIPTS/EnemySpawner/EnemySpawner.cs
+++ TADDIV - Hellstaurant/Assets/SCRIPTS/EnemySpawner/EnemySpawner.cs
@@ -2,17 +2,31 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class EnemySpawner : MonoBehaviour
-{
-    // Start is called before the first frame update
-    void Start()
-    {
-        
-    }
+public class EnemySpawner : MonoBehaviour{
+  
+[SerializeField] private float spawnRate = 1f;
 
-    // Update is called once per frame
-    void Update()
-    {
-        
-    }
+[SerializeField] private GameObject[] enemyPrefabs;
+
+[SerializeField] private bool canSpawn = true;
+
+private void Start () {
+	StartCoroutine(Spawner());
+	}
+
+private IEnumerator Spawner () {
+	WaitForSeconds wait = new WaitForSeconds(spawnRate);
+
+	while (canSpawn) {
+		yield return wait;
+		int rand = Random.Range(0, enemyPrefabs.Length);
+		GameObject enemyToSpawn = enemyPrefabs[rand];
+
+		Instantiate(enemyToSpawn, transform.position, Quaternion.identity);
+	}
+
+
+
 }
+}
+
