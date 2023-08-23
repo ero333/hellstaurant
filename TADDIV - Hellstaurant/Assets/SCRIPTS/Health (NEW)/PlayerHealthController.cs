17ddb6243c9c73d50069dda2d181c049ba8d3a03
IviiC21 <casal.ivandaniel@usal.edu.ar>
2023-08-23 17:12:02 -0300

    SISTEMA DE VIDA
    + Solución de problemas anteriores del sistema
    + Implementación del sistema de vida al completo y funcional
    
    Co-Authored-By: IgnaIsasMica <111125687+IgnaIsasMica@users.noreply.github.com>
    Co-Authored-By: camilaochoa10 <142224357+camilaochoa10@users.noreply.github.com>

--- TADDIV - Hellstaurant/Assets/SCRIPTS/Health (NEW)/PlayerHealthController.cs
+++ TADDIV - Hellstaurant/Assets/SCRIPTS/Health (NEW)/PlayerHealthController.cs
@@ -1,30 +1,51 @@
-// using System.Collections;
-// using System.Collections.Generic;
-// using UnityEngine;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
 
-// public class PlayerHealthController : MonoBehaviour
-// {
-//     public int currentHealth, maxHealth;
+public class PlayerHealthController : MonoBehaviour
+{
+    public int currentHealth, maxHealth; //Referencias a la vida actual y la vida llena
 
-//     // Start is called before the first frame update
-//     void Start()
-//     {
-//         currentHealth = maxHealth;
-//     }
+    public UIController controlUI; // Referencia al controlador de la UI del canvas
 
-//     // Update is called once per frame
-//     void Update()
-//     {
+    public static PlayerHealthController instance; // Instancia este script para poder ser llamado desde otros
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        currentHealth = maxHealth; //La vida actual se iguala a la vida máxima ni bien iniciamos el juego
+    }
+
+    private void Awake()
+    {
+        instance = this; //Activa la instancia para ser llamado
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+
+    public void OnTriggerEnter2D(Collider2D other)
+    {
+        if (other.CompareTag("Enemy"))
+        {
+            DealDamage(); //Cuando colisiona con un objeto con el tag Enemy, llama a DealDamage
+        }
         
-//     }
+    }
 
-//     public void DealDamage()
-//     {
-//         currentHealth--;
+    public void DealDamage()
+    {
+        currentHealth--; //Resta 1 punto de vida 
 
-//         if(currentHealth <= 0)
-//         {
-//             gameObject.SetActive(false);
-//         }
-//     }
-// }
+        UIController.Instance.UpdateHealthDisplay(); //Actualiza los corazones
+
+        if (currentHealth <= 0)
+        {
+            gameObject.SetActive(false); //Cuando la vida es 0 desactiva al jugador
+        }
+    }
+
+}
