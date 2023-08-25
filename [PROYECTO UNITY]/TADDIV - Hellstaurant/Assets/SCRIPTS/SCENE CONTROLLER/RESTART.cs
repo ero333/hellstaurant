c03da6d283cff4c64ffe7bdae9bde7c509639ee1
IviiC21 <casal.ivandaniel@usal.edu.ar>
2023-08-25 10:43:42 -0300

    PROYECTO SIN META

---
+++ [PROYECTO UNITY]/TADDIV - Hellstaurant/Assets/SCRIPTS/SCENE CONTROLLER/RESTART.cs
@@ -0,0 +1,25 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class RESTART : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    public void RestartScene()
+    {
+        Time.timeScale = 1;
+        UnityEngine.SceneManagement.SceneManager.LoadScene(UnityEngine.SceneManagement.SceneManager.GetActiveScene().buildIndex);
+    }
+}
