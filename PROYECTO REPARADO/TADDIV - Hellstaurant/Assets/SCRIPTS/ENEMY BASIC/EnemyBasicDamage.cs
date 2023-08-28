ec3e9d55b173069171a2809f822c49c936672c25
Camila Ochoa <c.ochoa1@usal.edu.ar>
2023-08-28 19:10:51 -0300

    + Drop monedas al matar enemigos

--- PROYECTO REPARADO/TADDIV - Hellstaurant/Assets/SCRIPTS/ENEMY BASIC/EnemyBasicDamage.cs
+++ PROYECTO REPARADO/TADDIV - Hellstaurant/Assets/SCRIPTS/ENEMY BASIC/EnemyBasicDamage.cs
@@ -5,16 +5,12 @@ using UnityEngine;
 
 public class EnemyBasicDamage : MonoBehaviour
 {
-
     public float enemyLife; // Variable de la vida del enemigo
-
     public int puntosEnemigo;
-
     public GameObject contadorPuntos;
-
     public Puntaje puntaje;
-
-    
+    [Range(0, 100)] public float chanceToDrop; // enemigo dropea monedas
+    public GameObject collectible; // lo que vamos a dropear
 
     void Start()
     {
@@ -22,30 +18,35 @@ public class EnemyBasicDamage : MonoBehaviour
         puntaje = contadorPuntos.GetComponent<Puntaje>();
     }
 
-    // Update is called once per frame
     void Update()
     {
-        
+
     }
 
     public void OnTriggerEnter2D(Collider2D other) // Cuando colisionan la pizza y el enemigo
     {
-        if (other.CompareTag("PizzaShoot")) //Si la pizza tiene el tag PizzaShoot
+        if (other.CompareTag("PizzaShoot")) // Si la pizza tiene el tag PizzaShoot
         {
-            enemyLife--; //Le baja 1 de vida al enemigo
-            Destroy(other.gameObject); //Destruye la pizza que le pegó
+            enemyLife--; // Le baja 1 de vida al enemigo
+            Destroy(other.gameObject); // Destruye la pizza que le pegó
 
-            if (enemyLife <= 0) 
+            if (enemyLife <= 0)
             {
                 onDeath();
             }
         }
-
     }
 
     public void onDeath()
     {
         puntaje.obtenerPuntaje(puntosEnemigo);
-        Destroy(gameObject); //Cuando la vida es menor o igual a cero se destruye el enemigo
+        Destroy(gameObject); // Cuando la vida es menor o igual a cero se destruye el enemigo
+
+        float dropSelect = Random.Range(0, 100f);
+
+        if (dropSelect <= chanceToDrop) //aca salen las monedas 
+        {
+            Instantiate(collectible, transform.position, Quaternion.identity); // Instancia en la posición del enemigo
+        }
     }
 }
