9b6381d9910130c9ee16252e6cdac104cd3d69f4
Camila Ochoa <c.ochoa1@usal.edu.ar>
2023-08-31 19:55:38 -0300

    + Codigo de disparo de enemigo (terminado) +Agregado de animacion de disparo

---
+++ TADDIV - Hellstaurant/Assets/SCRIPTS/ENEMY BASIC/EnemyBullet.cs
@@ -0,0 +1,48 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EnemyBullet : MonoBehaviour
+{
+    private Rigidbody2D rb;
+    public float force;
+    private float timer;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        rb = GetComponent<Rigidbody2D>();
+        Vector3 direction = PlayerHealthController.instance.transform.position - transform.position;
+        rb.velocity = new Vector2(direction.x, direction.y).normalized * force;
+
+        float rot = Mathf.Atan2(-direction.y, -direction.x) * Mathf.Rad2Deg;
+        transform.rotation = Quaternion.Euler(0, 0, rot + 90);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        timer += Time.deltaTime;
+
+        if (timer > 8)
+        {
+            Destroy(gameObject);
+        }
+    }
+
+    void OnTriggerEnter2D(Collider2D other)
+    {
+        if (other.gameObject.CompareTag("Player"))
+        {
+            PlayerHealthController playerHealth = other.gameObject.GetComponent<PlayerHealthController>();
+            if (playerHealth != null)
+            {
+                playerHealth.DealDamage(); // balita q hace daño
+            }
+            
+            Destroy(gameObject);
+        }
+    }
+}
+
+
---
+++ TADDIV - Hellstaurant/Assets/SCRIPTS/ENEMY BASIC/EnemyController.cs
@@ -0,0 +1,28 @@
+using UnityEngine;
+
+public class EnemyController : MonoBehaviour
+{
+    public Transform player; // Asigna el objeto del jugador desde el Editor de Unity
+    public float shootingDistance = 5f; // Distancia a la que el enemigo comenzará a disparar
+
+    private Animator animator;
+
+    private void Start()
+    {
+        animator = GetComponent<Animator>();
+    }
+
+    private void Update()
+    {
+        // Calcula la distancia entre el enemigo y el jugador
+        float distanceToPlayer = Vector3.Distance(transform.position, player.position);
+
+        // Si la distancia es menor o igual a la distancia de disparo, cambia al estado de disparar
+        if (distanceToPlayer <= shootingDistance)
+        {
+            // Activar la transición al estado de disparar
+            animator.SetTrigger("StartShooting");
+        }
+    }
+}
+
