80727b1023cdacefd2d591c347e87dd7185d519f
k4teg <k.giani@usal.edu.ar>
2023-09-02 19:26:38 -0300

    + Spawn de fugazzetta sobre la mesa

---
+++ TADDIV - Hellstaurant/Assets/SpawnPotenciadores.cs
@@ -0,0 +1,42 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SpawnPotenciadores : MonoBehaviour
+
+{
+    public float spawnRate = 1f;
+
+    public GameObject[] PowerUp;
+
+    public bool canSpawn = true;
+
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        StartCoroutine (PowerUpSpawn());
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    IEnumerator PowerUpSpawn ()
+    {
+        while (canSpawn)
+        {
+            WaitForSeconds wait = new WaitForSeconds(spawnRate);
+
+            yield return wait;
+            int rand = Random.Range(0, PowerUp.Length);
+            GameObject PowerUpSp = PowerUp[rand];
+
+
+            Instantiate(PowerUpSp, transform.position, Quaternion.identity);
+        }
+        
+    }
+}
