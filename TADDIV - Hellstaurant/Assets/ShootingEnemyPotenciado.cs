2c8c07404238909814eb0937840244326c63b47e
k4teg <k.giani@usal.edu.ar>
2023-09-16 16:14:10 -0300

    +aplicado los spritesheets potenciados a todos los enemigos

---
+++ TADDIV - Hellstaurant/Assets/ShootingEnemyPotenciado.cs
@@ -0,0 +1,76 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ShootingEnemyPotenciado : MonoBehaviour
+{
+    public float moveSpeed = 3f;
+    public float shootingRange = 5f;
+    public GameObject projectilePrefab;
+    public float projectileSpeed = 10f;
+    public float shootingCooldown = 2f;
+
+    public Animator animator;
+
+    private Transform player;
+
+    private float lastShotTime = 0f;
+
+    private void Start()
+    {
+        player = GameObject.FindGameObjectWithTag("Player").transform;
+        animator = GetComponent<Animator>();
+    }
+
+    private void Update()
+    {
+        float distanceToPlayer = Vector2.Distance(transform.position, player.position);
+
+        if (distanceToPlayer <= shootingRange)
+        {
+
+            StopMoving();
+            Shoot();
+        }
+        else
+        {
+
+            moving();
+        }
+    }
+
+    private void moving()
+    {
+        Vector2 moveDirection = (player.position - transform.position).normalized;
+        transform.Translate(moveDirection * moveSpeed * Time.deltaTime);
+    }
+
+    private void StopMoving()
+    {
+        GetComponent<Rigidbody2D>().velocity = Vector2.zero;
+    }
+
+    private void Shoot()
+    {
+        if (Time.time - lastShotTime > shootingCooldown)
+        {
+            animator.Play("EnemyShootsAtaquePotenciado");
+            animator.SetBool("PotenciadoShooting", true);
+            Vector2 shootDirection = (player.position - transform.position).normalized;
+            GameObject newProjectile = Instantiate(projectilePrefab, transform.position, Quaternion.identity);
+            Rigidbody2D projectileRb = newProjectile.GetComponent<Rigidbody2D>();
+            if (projectileRb != null)
+            {
+                projectileRb.velocity = shootDirection * projectileSpeed;
+            }
+            lastShotTime = Time.time;
+        }
+    }
+
+    private void OnDrawGizmosSelected()
+    {
+        Gizmos.color = Color.red;
+        Gizmos.DrawWireSphere(transform.position, shootingRange);
+    }
+}
+
