aba706c356a4dcfbfd7267c35b78cf9a67518c7d
IviiC21 <casal.ivandaniel@usal.edu.ar>
2023-09-17 23:49:36 -0300

    BOTON CONTINUAR
    + Botón de continuar aparece y desaparece segun el save que haya
    
    Co-Authored-By: Kate <142017706+k4teg@users.noreply.github.com>

--- TADDIV - Hellstaurant/Assets/SCRIPTS/SCENE CONTROLLER/PLAYMENU.cs
+++ TADDIV - Hellstaurant/Assets/SCRIPTS/SCENE CONTROLLER/PLAYMENU.cs
@@ -5,26 +5,41 @@ using UnityEngine.SceneManagement;
 
 public class PLAYMENU : MonoBehaviour
 {
+
+    public int hayPartida;
+
+    public GameObject botonContinuar;
+
     // Start is called before the first frame update
     void Start()
     {
+        hayPartida = PlayerPrefs.GetInt("PartidaGuardada");
         
+
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+
     }
 
     public void jugar()
     {
         SceneManager.LoadScene("Selector de Niv");
+
+        PlayerPrefs.SetInt("PartidaGuardada", 1);
+
+        botonContinuar.SetActive(true);
     }
 
     public void Reset()
     {
         PlayerPrefs.DeleteAll();
+
+        PlayerPrefs.SetInt("PartidaGuardada", 0);
+
+        botonContinuar.SetActive(false);
     }
 
     public void Tienda()
