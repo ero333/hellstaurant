e155ac11a66a57cfa8c9788167eb1c2b83e0fdad
Camila Ochoa <c.ochoa1@usal.edu.ar>
2023-09-26 16:58:36 -0300

    + Agregado sprites personaje mujer + Cambiar skin al tocar boton codigo (en proceso)

---
+++ TADDIV - Hellstaurant/Assets/SCRIPTS/SkinsSelector/PlayerSpawner.cs
@@ -0,0 +1,15 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerSpawner : MonoBehaviour
+{
+    
+    void Start()
+    {
+        Instantiate(PlayerStorage.playerPrefab, this.transform.position, this.transform.rotation);
+        Destroy(this.gameObject);
+    }
+
+    
+}
