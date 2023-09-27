56dda89621a6baff422d608eaed0dde5d9dfb312
Camila Ochoa <c.ochoa1@usal.edu.ar>
2023-09-27 16:26:01 -0300

    + codigo de skins en proceso
    Co-Authored-By: AzulDiStasio <142017743+AzulDiStasio@users.noreply.github.com>

---
+++ TADDIV - Hellstaurant/Assets/SCRIPTS/PLAYER/PlayerSkin.cs
@@ -0,0 +1,54 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerSkin : MonoBehaviour
+{
+
+    public AnimatorOverrideController girlSkin;
+    public RuntimeAnimatorController defaultSkin;
+    public Animator anim;
+
+    public string skinName;
+
+
+    // Start is called before the first frame update
+    void Start()
+    {
+
+        anim = GetComponent<Animator>();
+        defaultSkin = anim.runtimeAnimatorController;
+
+        skinName = PlayerPrefs.GetString("skin seleccionada");
+
+        if (skinName == "girlSkin")
+        {
+            girlSkinchange();
+        }
+
+        if (skinName == "manSkin")
+        {
+            manSkinchange();
+        }
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+
+    }
+
+    public void girlSkinchange()
+    {
+        anim.runtimeAnimatorController = girlSkin as RuntimeAnimatorController;
+    }
+
+    public void manSkinchange()
+    {
+        anim.runtimeAnimatorController = defaultSkin as RuntimeAnimatorController;
+    }
+
+
+}
