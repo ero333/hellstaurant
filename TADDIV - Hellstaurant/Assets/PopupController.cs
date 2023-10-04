0dbb58284944acbad435aebae71b948de71d1533
IviiC21 <casal.ivandaniel@usal.edu.ar>
2023-10-04 18:48:51 -0300

    MENU PRINCIPAL NAVEGABLE CON MANDO Y TECLADO
    Co-Authored-By: XtR-Software <112583777+XtR-Software@users.noreply.github.com>

--- TADDIV - Hellstaurant/Assets/PopupController.cs
+++ TADDIV - Hellstaurant/Assets/PopupController.cs
@@ -2,19 +2,26 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.UI;
+using UnityEngine.InputSystem;
+using UnityEngine.EventSystems;
 
 public class PopupController : MonoBehaviour
 {
     public GameObject popupPanel;
+    public GameObject ventanaEmergente;
+    public GameObject menuPrincipal;
+
 
     public void OpenPopup()
     {
         popupPanel.SetActive(true);
+        EventSystem.current.SetSelectedGameObject(ventanaEmergente.GetComponentInChildren<Button>().gameObject);
     }
 
     public void ClosePopup()
     {
         popupPanel.SetActive(false);
+        EventSystem.current.SetSelectedGameObject(menuPrincipal.GetComponentInChildren<Button>().gameObject);
     }
 
     public void OnYesButtonClick()
