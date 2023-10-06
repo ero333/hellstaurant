f1f65242ee81d7fc8f75268a1bff1ade10dd6ff6
k4teg <k.giani@usal.edu.ar>
2023-10-06 14:37:21 -0300

    +buscada e implementada la cancion de fondo para los menus

---
+++ TADDIV - Hellstaurant/Assets/backgroundmusic.cs
@@ -0,0 +1,28 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class backgroundmusic : MonoBehaviour
+{
+    private static backgroundmusic instancia;
+
+    private void Awake()
+    {
+        // Verifica si ya existe una instancia de ControlDeMusica
+        if (instancia != null && instancia != this)
+        {
+            // Destruye este objeto si ya hay una instancia
+            Destroy(gameObject);
+            return;
+        }
+
+        // Marca este objeto como persistente entre escenas
+        DontDestroyOnLoad(gameObject);
+
+        // Asigna esta instancia como la única instancia
+        instancia = this;
+    }
+
+    // Lógica para reproducir, pausar, detener la música, etc.
+    // Puedes usar un componente AudioSource para reproducir la música.
+}
