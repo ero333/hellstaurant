8b78ed72ee0331295310b627d5e1c73ca0c49ab9
IgnaIsasMica <ArgentISM@gmail.com>
2023-10-08 14:23:00 -0300

    script de tiempo para endless y borro barra de tiempo del endless

---
+++ TADDIV - Hellstaurant/Assets/Endlesstimer.cs
@@ -0,0 +1,73 @@
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using TMPro;
+
+public class Endlesstimer : MonoBehaviour
+{
+    [SerializeField] private int startingTime;
+    [SerializeField] private float TimeUntilHourChange;
+    [SerializeField] private TMP_Text timeText;
+
+    [NonSerialized] public int timeHours;
+
+    public bool playerAlive;
+
+    public GameObject WIN;
+    private Animator WinAnimator;
+
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        playerAlive = true;
+        timeHours = startingTime;
+
+
+        StartCoroutine(routine: advanceHourOverTime());
+
+
+
+
+    }
+
+    // Update is called once per frame
+    private void Update()
+    {
+        timeText.text = timeHours + ":00 PM";
+
+
+    }
+
+    private IEnumerator advanceHourOverTime()
+    {
+        if (playerAlive)
+        {
+            yield return new WaitForSeconds(TimeUntilHourChange);
+
+            if (timeHours == 12)
+                timeHours = 1;
+            else
+                timeHours++;
+
+
+        }
+        // else
+       // {
+         //   stopTimer();
+        //}
+
+    }
+
+    public void stopTimer()
+    {
+        if (!playerAlive)
+        {
+            StopAllCoroutines();
+            timeText.text = timeHours + ":00 PM";
+        }
+    }
+
+
+}
