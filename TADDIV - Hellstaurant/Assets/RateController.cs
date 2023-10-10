5ecc0c0ef84e675491a795324b3b261fc543e032
k4teg <k.giani@usal.edu.ar>
2023-10-10 20:31:26 -0300

    +implementado al 100 el sistema de raiting (pop up, estrellas funcionales, botones, etc)
    Co-Authored-By: IviiC21 <142019326+IviiC21@users.noreply.github.com.>

---
+++ TADDIV - Hellstaurant/Assets/RateController.cs
@@ -0,0 +1,261 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class RateController : MonoBehaviour
+{
+    
+    public GameObject botonRate;
+    public int rateado;
+    public GameObject popupRate;
+
+    public Sprite estrellavacia;
+    public Sprite estrellallena;
+
+    public int ratinggame;
+    public int ratingart;
+
+    [Header("botones estrellas juego")]
+    public Button estrella1;
+    public Button estrella2;
+    public Button estrella3;
+    public Button estrella4;
+    public Button estrella5;
+
+    [Header("botones estrellas arte")]
+    public Button estrellart1;
+    public Button estrellart2;
+    public Button estrellart3;
+    public Button estrellart4;
+    public Button estrellart5;
+
+
+    void Start()
+    {
+
+        rateado = PlayerPrefs.GetInt("rateado");
+        popupRate.SetActive(false);
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if(rateado >= 1)
+        {
+            botonRate.SetActive(false);
+        }
+    }
+
+    public void UnoEstrella()
+    {
+        
+        Image estrella1image=estrella1.GetComponent<Image>();
+        Image estrella2image = estrella2.GetComponent<Image>();
+        Image estrella3image = estrella3.GetComponent<Image>();
+        Image estrella4image = estrella4.GetComponent<Image>();
+        Image estrella5image = estrella5.GetComponent<Image>();
+
+
+        estrella1image.sprite = estrellallena;
+        estrella2image.sprite = estrellavacia;
+        estrella3image.sprite = estrellavacia;
+        estrella4image.sprite = estrellavacia;
+        estrella5image.sprite = estrellavacia;
+
+        ratinggame = 1;
+    }
+
+    public void DosEstrella()
+    {
+
+        Image estrella1image = estrella1.GetComponent<Image>();
+        Image estrella2image = estrella2.GetComponent<Image>();
+        Image estrella3image = estrella3.GetComponent<Image>();
+        Image estrella4image = estrella4.GetComponent<Image>();
+        Image estrella5image = estrella5.GetComponent<Image>();
+
+
+        estrella1image.sprite = estrellallena;
+        estrella2image.sprite = estrellallena;
+        estrella3image.sprite = estrellavacia;
+        estrella4image.sprite = estrellavacia;
+        estrella5image.sprite = estrellavacia;
+
+        ratinggame = 2;
+    }
+
+    public void TresEstrella()
+    {
+
+        Image estrella1image = estrella1.GetComponent<Image>();
+        Image estrella2image = estrella2.GetComponent<Image>();
+        Image estrella3image = estrella3.GetComponent<Image>();
+        Image estrella4image = estrella4.GetComponent<Image>();
+        Image estrella5image = estrella5.GetComponent<Image>();
+
+
+        estrella1image.sprite = estrellallena;
+        estrella2image.sprite = estrellallena;
+        estrella3image.sprite = estrellallena;
+        estrella4image.sprite = estrellavacia;
+        estrella5image.sprite = estrellavacia;
+
+        ratinggame = 3;
+    }
+
+    public void CuatroEstrella()
+    {
+
+        Image estrella1image = estrella1.GetComponent<Image>();
+        Image estrella2image = estrella2.GetComponent<Image>();
+        Image estrella3image = estrella3.GetComponent<Image>();
+        Image estrella4image = estrella4.GetComponent<Image>();
+        Image estrella5image = estrella5.GetComponent<Image>();
+
+
+        estrella1image.sprite = estrellallena;
+        estrella2image.sprite = estrellallena;
+        estrella3image.sprite = estrellallena;
+        estrella4image.sprite = estrellallena;
+        estrella5image.sprite = estrellavacia;
+
+        ratinggame = 4;
+    }
+
+    public void CincoEstrella()
+    {
+
+        Image estrella1image = estrella1.GetComponent<Image>();
+        Image estrella2image = estrella2.GetComponent<Image>();
+        Image estrella3image = estrella3.GetComponent<Image>();
+        Image estrella4image = estrella4.GetComponent<Image>();
+        Image estrella5image = estrella5.GetComponent<Image>();
+
+
+        estrella1image.sprite = estrellallena;
+        estrella2image.sprite = estrellallena;
+        estrella3image.sprite = estrellallena;
+        estrella4image.sprite = estrellallena;
+        estrella5image.sprite = estrellallena;
+
+        ratinggame = 5;
+    }
+
+    //arte
+
+    public void UnoEstrellart()
+    {
+
+        Image estrellart1image = estrellart1.GetComponent<Image>();
+        Image estrellart2image = estrellart2.GetComponent<Image>();
+        Image estrellart3image = estrellart3.GetComponent<Image>();
+        Image estrellart4image = estrellart4.GetComponent<Image>();
+        Image estrellart5image = estrellart5.GetComponent<Image>();
+
+
+        estrellart1image.sprite = estrellallena;
+        estrellart2image.sprite = estrellavacia;
+        estrellart3image.sprite = estrellavacia;
+        estrellart4image.sprite = estrellavacia;
+        estrellart5image.sprite = estrellavacia;
+
+        ratingart = 1;
+    }
+
+    public void DosEstrellart()
+    {
+
+        Image estrellart1image = estrellart1.GetComponent<Image>();
+        Image estrellart2image = estrellart2.GetComponent<Image>();
+        Image estrellart3image = estrellart3.GetComponent<Image>();
+        Image estrellart4image = estrellart4.GetComponent<Image>();
+        Image estrellart5image = estrellart5.GetComponent<Image>();
+
+
+        estrellart1image.sprite = estrellallena;
+        estrellart2image.sprite = estrellallena;
+        estrellart3image.sprite = estrellavacia;
+        estrellart4image.sprite = estrellavacia;
+        estrellart5image.sprite = estrellavacia;
+
+        ratingart = 2;
+    }
+
+    public void TresEstrellart()
+    {
+
+        Image estrellart1image = estrellart1.GetComponent<Image>();
+        Image estrellart2image = estrellart2.GetComponent<Image>();
+        Image estrellart3image = estrellart3.GetComponent<Image>();
+        Image estrellart4image = estrellart4.GetComponent<Image>();
+        Image estrellart5image = estrellart5.GetComponent<Image>();
+
+
+        estrellart1image.sprite = estrellallena;
+        estrellart2image.sprite = estrellallena;
+        estrellart3image.sprite = estrellallena;
+        estrellart4image.sprite = estrellavacia;
+        estrellart5image.sprite = estrellavacia;
+
+        ratingart = 3;
+    }
+
+    public void CuatroEstrellart()
+    {
+
+        Image estrellart1image = estrellart1.GetComponent<Image>();
+        Image estrellart2image = estrellart2.GetComponent<Image>();
+        Image estrellart3image = estrellart3.GetComponent<Image>();
+        Image estrellart4image = estrellart4.GetComponent<Image>();
+        Image estrellart5image = estrellart5.GetComponent<Image>();
+
+
+        estrellart1image.sprite = estrellallena;
+        estrellart2image.sprite = estrellallena;
+        estrellart3image.sprite = estrellallena;
+        estrellart4image.sprite = estrellallena;
+        estrellart5image.sprite = estrellavacia;
+
+        ratingart = 4;
+    }
+
+    public void CincoEstrellart()
+    {
+
+        Image estrellart1image = estrellart1.GetComponent<Image>();
+        Image estrellart2image = estrellart2.GetComponent<Image>();
+        Image estrellart3image = estrellart3.GetComponent<Image>();
+        Image estrellart4image = estrellart4.GetComponent<Image>();
+        Image estrellart5image = estrellart5.GetComponent<Image>();
+
+
+        estrellart1image.sprite = estrellallena;
+        estrellart2image.sprite = estrellallena;
+        estrellart3image.sprite = estrellallena;
+        estrellart4image.sprite = estrellallena;
+        estrellart5image.sprite = estrellallena;
+
+        ratingart = 5;
+    }
+
+    public void sendRate()
+    {
+        PlayerPrefs.SetInt("puntajeJuego =", ratinggame);
+        PlayerPrefs.SetInt("puntajeArte =", ratingart);
+        popupRate.SetActive(false);
+        botonRate.SetActive(false);
+        PlayerPrefs.SetInt("rateado", 1);
+    }
+
+    public void cerrarRate()
+    {
+        popupRate.SetActive(false);
+    }
+
+    public void abrirRate()
+    { 
+        popupRate.SetActive(true); 
+    }
+}
