17ea519c3cc4cee69015a07afd1e9e9876294c92
IviiC21 <142019326+IviiC21@users.noreply.github.com>
2023-11-10 08:26:39 -0300

    Update StarAmount.cs

--- TADDIV - Hellstaurant/Assets/SCRIPTS/PLAYER/StarAmount.cs
+++ TADDIV - Hellstaurant/Assets/SCRIPTS/PLAYER/StarAmount.cs
@@ -371,7 +371,7 @@ public class StarAmount : MonoBehaviour
            {"rapidoPHit", healthController.rapidoPHit},
            {"rodeteHit", healthController.rodeteHit},
            {"rodetePHit", healthController.rodetePHit},
-           {"aranaHit", healthController.arañaHit},
+           {"aranaHit", healthController.aranaHit},
            {"cucarachaHit", healthController.cucarachaHit},
            {"jefeHit", healthController.bossHit}
 
@@ -390,7 +390,7 @@ public class StarAmount : MonoBehaviour
         Debug.Log("LevelComplete.rapidoPHit =" + healthController.rapidoPHit);
         Debug.Log("LevelComplete.rodeteHit =" + healthController.rodeteHit);
         Debug.Log("LevelComplete.rodetePHit =" + healthController.rodetePHit);
-        Debug.Log("LevelComplete.aranaHit =" + healthController.arañaHit);
+        Debug.Log("LevelComplete.aranaHit =" + healthController.aranaHit);
         Debug.Log("LevelComplete.cucarachaHit =" + healthController.cucarachaHit);
         Debug.Log("LevelComplete.jefeHit =" + healthController.bossHit);
 
