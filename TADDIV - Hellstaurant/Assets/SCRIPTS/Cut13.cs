99189cef849574ec53072de4d33ba9987ebf48b5
Brunobon <bruno.bonsignore@usal.edu.ar>
2024-01-23 10:11:58 -0300

    + Agregar todas las escenas de las cutscenes restantes + Crear todos los scripts para las cutscenes 3-13 + Implementar Cutscenes 3-5

---
+++ TADDIV - Hellstaurant/Assets/SCRIPTS/Cut13.cs
@@ -0,0 +1,81 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.EventSystems;
+using UnityEngine.SceneManagement;
+using UnityEngine.UI;
+
+public class Cut13 : MonoBehaviour
+{
+    public GameObject animacion1, animacion2, animacion3, animacion4, animacion5, animacion6;
+
+    public GameObject botonSiguiente;
+
+
+
+
+    public float contadorAnim;
+
+    void Start()
+    {
+        contadorAnim = 0;
+        animacion1.SetActive(true);
+
+        EventSystem.current.SetSelectedGameObject(botonSiguiente.GetComponentInChildren<Button>().gameObject);
+    }
+
+    public void pasarAnim()
+    {
+        contadorAnim++;
+
+        if (contadorAnim == 1)
+        {
+            animacion1.SetActive(false);
+            animacion2.SetActive(true);
+        }
+
+        if (contadorAnim == 2)
+        {
+            animacion2.SetActive(false);
+            animacion3.SetActive(true);
+        }
+        if (contadorAnim == 3)
+        {
+            animacion3.SetActive(false);
+            animacion4.SetActive(true);
+        }
+        if (contadorAnim == 4)
+        {
+            animacion4.SetActive(false);
+            animacion5.SetActive(true);
+        }
+        if (contadorAnim == 5)
+        {
+            animacion5.SetActive(false);
+            animacion6.SetActive(true);
+        }
+
+        if (contadorAnim == 6)
+        {
+            SceneManager.LoadScene("Nivel 12");
+        }
+
+       
+
+    }
+
+
+    public void desactivarBoton()
+    {
+        botonSiguiente.SetActive(false);
+
+    }
+
+    public void activarBoton()
+    {
+        botonSiguiente.SetActive(true);
+
+    }
+
+
+}
